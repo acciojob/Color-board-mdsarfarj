09971bd7fca52b284eb2abc09062b030a5906f23
Md Sarfraj <116196482+mdsarfarj@users.noreply.github.com>
2023-11-22 18:48:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const colorBoard = document.getElementById("color-board");
+
+for (let i = 0; i < 800; i++) {
+  const square = document.createElement("div");
+  square.className = "square";
+  colorBoard.appendChild(square);
+}
+
+const handleMouseEnter = (e) => {
+  e.target.style.backgroundColor = "#fff";
+};
+
+const handleMouseLeave = (e) => {
+  e.target.style.backgroundColor = "#000";
+};
+
+for (let i = 0; i < 800; i++) {
+  const square = document.getElementsByClassName("square")[i];
+  square.addEventListener("mouseenter", handleMouseEnter);
+  square.addEventListener("mouseleave", handleMouseLeave);
+}
